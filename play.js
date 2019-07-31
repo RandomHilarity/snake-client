94afaeb36648a65135049e9b252aaa55f713f386
John Webster <johnrwebs@gmail.com>
2019-07-31 16:08:45 +0000

    initial commit, added play.js with basic connect to server functionality

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes connection with game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // handle messages incoming from server
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+
+    return conn;
+  });
+};
+
+
+console.log("Connecting ...");
+connect();
