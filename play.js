db91fe247cfda4035e5bf47f33a2c64365ca578b
John Webster <johnrwebs@gmail.com>
2019-07-31 16:25:42 +0000

    second commit, created client module

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require("net");
-
 // establishes connection with game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // handle messages incoming from server
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-
-    return conn;
-  });
-};
-
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
