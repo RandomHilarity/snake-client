ae89dc4996c7fab66cc41c4853ef80691dc0d5e5
John Webster <johnrwebs@gmail.com>
2019-07-31 21:02:58 +0000

    added message on q

--- input.js
+++ input.js
@@ -29,6 +29,9 @@ const handleUserInput = function(key) {
   if (key === 'd') {
     connection.write("Move: right");
   }
+  if (key === 'q') {
+    connection.write("Say: I am snek!");
+  }
 };
 
 module.exports = {setupInput};
