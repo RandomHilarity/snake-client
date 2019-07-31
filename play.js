e2a4435bb1b51a280eee566341b49e9355a1a484
John Webster <johnrwebs@gmail.com>
2019-07-31 19:02:42 +0000

    added keyboard input capability and CtrlC to close

--- play.js
+++ play.js
@@ -3,3 +3,31 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+setupInput();
+
+const handleUserInput = function(key) {
+  
+  //exits program if "CtrlC + C" triggered
+  if (key === '\u0003') {
+    console.log("CTRL+C entered!  Exiting.");
+    process.exit();
+  }
+};
+
+
+
+  
+
+
+
